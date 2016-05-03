0cb34897aa257877ea872c079e07958e52b305e1
scfhao <scfhao@126.com>
2016-05-03 20:12:18 +0800

    Initial Commit

---
+++ SODownloadExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SODownloadExample
+//
+//  Created by xueyi on 16/5/3.
+//  Copyright © 2016年 http://scfhao.coding.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SODownloadExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SODownloadExample
+//
+//  Created by xueyi on 16/5/3.
+//  Copyright © 2016年 http://scfhao.coding.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
